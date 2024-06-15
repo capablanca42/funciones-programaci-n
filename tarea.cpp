2dbaa9eca21073b8a965fc049c8299110fe8863b
Compu 05 <compu_05@luminar.edu.ec>
2024-06-14 19:40:02 -0500

    programacion

---
+++ tarea.cpp
@@ -0,0 +1,31 @@
+//Resuelva los siguientes ejercicios con funciones - bucles - condicionales.
+//1) Encuentre el valor de las siguientes expresiones, utilizando el depurador y la ventana watch:
+//Ejemplo: Para este código: cuál es el valor de la expresión a > 6 || b==3 ?
+//Y la ventana wach:
+//Ejemplo: para el codigo: es el valor de la expresión a > 6 || b==3 ?
+//Resultado de la expresión es true
+
+
+using namespace std;
+
+int main (){
+//valores iniciales: x=5, y =0;    expresión:  if (x==5 && y!=0)
+ int x=5,  y=0;   
+if(x==5 ||y!=0 ){
+    
+    cout<< "es verdadero";
+    cout<< "es falso";
+}else{
+    
+} 
+
+//b) valores iniciales: n=90;    expresión:  if (!(n>=1  &&  n<=100)
+int n=90;   
+cout << "is true";
+if (!(n>1 && n<=100)){
+cout << "is false";
+}else{
+  
+} 
+return 0;
+}
---
+++ tarea2.cpp
@@ -0,0 +1,14 @@
+
+using namespace std;
+
+int main (){
+ int x=5,  y=0;   
+if(x==5 ||y!=0 ){
+  return false;
+}else{
+    return true;
+} 
+
+
+return 0;
+}
